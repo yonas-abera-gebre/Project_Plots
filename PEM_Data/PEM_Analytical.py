ae2234cda312a5bf9be4a9e55cad2d2fef80ee2f
ygebre8 <ygebre1@hawk.iit.edu>
2022-02-17 16:11:55 -0700

    ad

--- PEM_Data/PEM_Analytical.py
+++ PEM_Data/PEM_Analytical.py
@@ -9,8 +9,8 @@ import scipy.special as special
 import mpmath as mp
 from scipy.signal import find_peaks
 from math import floor, ceil
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
 import sys
 
 def GBF(n,a,b):
@@ -66,19 +66,19 @@ def GBF_Zeros(k_array, quiver_energy, quiver_radius, Ip, omega):
 def PAD_2D(quiver_energy, quiver_radius, omega, polarization):
 
 
-    x_momentum = np.linspace(-2.0 , 2.0, 80)
-    y_momentum = np.linspace(-2.0 , 2.0, 80)
-    z_momentum = np.linspace(-2.0 , 2.0, 80)
+    x_momentum = np.linspace(-2.0 , 2.0, 40)
+    y_momentum = np.linspace(-2.0 , 2.0, 40)
+    z_momentum = np.linspace(-2.0 , 2.0, 40)
 
     
-    # pad_value = np.zeros((z_momentum.size,x_momentum.size))
-    pad_value_save = np.zeros((z_momentum.size,y_momentum.size, x_momentum.size))
+    pad_value = np.zeros((z_momentum.size,x_momentum.size))
+    # pad_value_save = np.zeros((z_momentum.size,y_momentum.size, x_momentum.size))
      
     for i, kx in enumerate(x_momentum):
         print(round(kx,3))
-        for j, ky in enumerate(y_momentum):
+        for j, kz in enumerate(z_momentum):
             pad_value_temp = 0.0
-            for l, kz in enumerate(z_momentum):
+            for l, ky in enumerate(y_momentum):
                 
                 k = np.sqrt(kx*kx + ky*ky + kz*kz)
                 N = Calculate_NPA(k, quiver_energy, Ip, omega)
@@ -87,30 +87,30 @@ def PAD_2D(quiver_energy, quiver_radius, omega, polarization):
                 mole_orientation = np.array([0,0,1])
                 quiver_vector = quiver_radius*polarization
                 
-                # pad_value_temp += k*pow(GBF(N, np.dot(quiver_vector, k_vector), quiver_energy/(2*omega)),2)*pow(quiver_energy - N*omega ,2)*pow(np.sin(np.dot(k_vector, mole_orientation)), 2)
+                pad_value_temp += k*pow(GBF(N, np.dot(quiver_vector, k_vector), quiver_energy/(2*omega)),2)*pow(quiver_energy - N*omega ,2)*pow(np.sin(np.dot(k_vector, mole_orientation)), 2)
 
-                pad_value_save[i,j,l] = k*pow(GBF(N, np.dot(quiver_vector, k_vector), quiver_energy/(2*omega)),2)*pow(quiver_energy - N*omega ,2)*pow(np.cos(np.dot(k_vector, mole_orientation)), 2)
+                # pad_value_save[i,j,l] = k*pow(GBF(N, np.dot(quiver_vector, k_vector), quiver_energy/(2*omega)),2)*pow(quiver_energy - N*omega ,2)*pow(np.cos(np.dot(k_vector, mole_orientation)), 2)
                 
-            # pad_value[j, i] += pad_value_temp
+            pad_value[j, i] += pad_value_temp
     
-    pad_value_save = pad_value_save / pad_value_save.max() 
-    arr_reshaped = pad_value_save.reshape(pad_value_save.shape[0], -1)
-    np.savetxt("Ana_Z_B_Two_Photon.txt", arr_reshaped)
+    # pad_value_save = pad_value_save / pad_value_save.max() 
+    # arr_reshaped = pad_value_save.reshape(pad_value_save.shape[0], -1)
+    # np.savetxt("Ana_Z_B_Two_Photon4.txt", arr_reshaped)
     
-    exit()
+    # exit()
     
-    # pad_value = pad_value / pad_value.max()
-    # pos = plt.imshow(pad_value, extent=[-2.0, 2.0, -2.0, 2.0])#, norm=LogNorm(vmin=1e-3, vmax=1))
-    # plt.colorbar(pos)
-    # plt.savefig("FIG_Z_XZ_AB.png")
+    pad_value = pad_value / pad_value.max()
+    pos = plt.imshow(pad_value, extent=[-2.0, 2.0, -2.0, 2.0])#, norm=LogNorm(vmin=1e-3, vmax=1))
+    plt.colorbar(pos)
+    plt.savefig("FIG.png")
     
 if __name__=="__main__":
     
     polarization = np.array([0,0,1])
     ellipticity = 0.
     ellipticity_vector = np.array([0,1,0])
-    intensity = 3.51e14
-    omega = 0.75
+    intensity = 1e14
+    omega = 1.5
     Ip = 1.0
     quiver_radius, quiver_energy = Calculate_Parameters(intensity, omega)
     PAD_2D(quiver_energy, quiver_radius, omega, polarization)
